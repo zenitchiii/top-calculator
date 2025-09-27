44b6a962db62d24e80be79ac51c04f9f0cc0d6e0
zenitchiii <105142307+zenitchiii@users.noreply.github.com>
2025-09-27 18:12:53 +0800

    setting up the interactivity

--- script.js
+++ script.js
@@ -17,7 +17,27 @@ function divide(num1,num2) {
 let num1;
 let num2;
 let operator;
+let input = "";
 
-function operate(num1, operator, num2) {
+function operate(equation, operation) {
     return;
 }
+
+function appendDigit(digit) {
+    input += digit;
+    display.value = input;
+}
+
+const display = document.getElementById("display");
+const digits = ["0","1","2","3","4","5","6","7","8","9"];
+
+digits.forEach(digit => {
+    document.getElementById(digit).addEventListener("click", () => {
+        appendDigit(digit);
+    });
+});
+
+document.getElementById("clear").addEventListener("click", () => {
+    input = "";
+    display.value = "";
+})
