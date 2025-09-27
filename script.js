27bb4c47166f84791ef7a8fae0b6d7c17770bab2
zenitchiii <105142307+zenitchiii@users.noreply.github.com>
2025-09-27 17:55:08 +0800

    setting up the functions

---
+++ script.js
@@ -0,0 +1,23 @@
+function add(num1,num2) {
+    return num1 + num2;
+}
+
+function subtract(num1,num2) {
+    return num1 - num2;
+}
+
+function multiply(num1,num2) {
+    return num1 * num2;
+}
+
+function divide(num1,num2) {
+    return num1 / num2;
+}
+
+let num1;
+let num2;
+let operator;
+
+function operate(num1, operator, num2) {
+    return;
+}
